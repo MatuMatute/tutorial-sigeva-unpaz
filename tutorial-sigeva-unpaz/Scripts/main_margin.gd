8c099a373c332faf9cb4bd0e2c073158f9db998a
Matías Sandoval <matiasgonzalezsandoval2@gmail.com>
2025-10-08 00:52:41 -0300

    0.2.0
    - El tutorial funciona de principio a fin
    - Hay cosas que arreglar y añadir

--- tutorial-sigeva-unpaz/Scripts/main_margin.gd
+++ tutorial-sigeva-unpaz/Scripts/main_margin.gd
@@ -4,16 +4,30 @@ extends MarginContainer
 @onready var mainContainer: VBoxContainer = $TextContainer/MainContainer;
 @onready var title: RichTextLabel = $TextContainer/MainContainer/Title;
 @onready var description: RichTextLabel = $TextContainer/MainContainer/Description;
-@onready var image: TextureRect = $TextContainer/MainContainer/Image;
+@onready var image: TextureRect = $TextContainer/MainContainer/ImageContainer/Image;
 @onready var main: Control = get_parent();
-@onready var mainButton: Button = $MainButton;
-@onready var infoButton: Button = $Info
+@onready var mainButton: Button = $ButtonContainer/MainButton;
+@onready var backButton: Button = $ButtonContainer/BackButton;
+@onready var infoButton: Button = $Info;
 
 const titleTexts: Array = [
 	"[b]Guía paso a paso[/b]\npara completar tu [b]CVAR[/b]",
 	"",
 	"
-	[b]¿Qué es CVAR?[/b]",
+	[b]¿Qué es CVar?[/b]",
+	"[b]Objetivos[/b]",
+	"[b]Formulario de registro[/b]",
+	"[b]Confirmación por mail[/b]",
+	"[b]Inicio de sesión[/b]",
+	"[b]Cambio de contraseña[/b]",
+	"[b]Selección de rol[/b]",
+	"",
+	"[b]Datos Personales[/b]",
+	"[b]Formación Académica[/b]",
+	"[b]Antecedentes en C y T[/b]",
+	"[b]Imprimir CV[/b]",
+	"[b]¡Felicidades![/b]
+Completaste el tutorial :)",
 	"PRUEBA1"
 ];
 
@@ -21,46 +35,130 @@ const descriptionTexts: Array = [
 	"Un [b]recorrido interactivo[/b] para que cargues tu información de manera simple y rápida.",
 	"",
 	"
-	El [b]Curriculum Vitae Argentino[/b] (CVAR) es una [b]herramienta digital[/b] donde los
-	investigadores, docentes y becarios registran su producción académica y profesional.
-	Tenerlo actualizado facilita tu participación en convocatorias, becas y proyectos de investigación.",
+		El [b]Curriculum Vitae Argentino[/b] (CVar) es una [b]herramienta digital[/b] donde los investigadores, docentes 
+		y becarios registran su producción académica y profesional.
+		Tenerlo actualizado facilita tu participación en convocatorias, becas y proyectos de investigación.",
+	"
+	
+[b]Módulo 1: Registro[/b]
+	- Llenar formulario de registro
+	- Confirmación por mail
+	- Inicio de sesión
+	- Cambio de contraseña
+	
+	
+[b]Módulo 2: Formulario[/b]
+	- Selección de rol
+	- Llenar formulario
+	- Imprimir CV",
+	"Usá esta guía todas las veces que necesites, está para acompañarte en el proceso.",
 	"PRUEBA1"
 ];
 
 const buttonTexts: Array = [
 	"¡Estoy listo/a!",
 	"¡Entendido!",
-	"Siguiente"
-]
+	"Siguiente",
+	"Finalizar",
+	"Comenzar de nuevo"
+];
+
+const images: Array = [
+	preload("res://Assets/Tutorial/formulario_registro.png"),
+	preload("res://Assets/Tutorial/comprobacion_mail.png"),
+	preload("res://Assets/Tutorial/inicio_sesion.png"),
+	preload("res://Assets/Tutorial/cambio_contraseña.png"),
+	preload("res://Assets/Tutorial/seleccion_rol.png"),
+	preload("res://Assets/Tutorial/datos_personales.png"),
+	preload("res://Assets/Tutorial/formacion_academica.png"),
+	preload("res://Assets/Tutorial/antecedentes_cyt.png"),
+	preload("res://Assets/Tutorial/imprimir_cv.png")
+];
+
+var textIndex: int;
 
 func _ready() -> void:
-	progressBar.hide();
+	backButton.hide();
 	title.text = titleTexts[0];
 	description.text = descriptionTexts[0];
 
+func _on_progress_bar_value_changed(value: float) -> void:
+	textIndex = int(value);
+	
+	match textIndex:
+		4:
+			$TextContainer/MainContainer/ImageContainer.show();
+			image.texture = images[0];
+			title.text = titleTexts[textIndex];
+		5:
+			image.texture = images[1];
+			title.text = titleTexts[textIndex];
+		6:
+			image.texture = images[2];
+			title.text = titleTexts[textIndex];
+		7:
+			image.texture = images[3];
+			title.text = titleTexts[textIndex];
+		8:
+			image.texture = images[4];
+			title.text = titleTexts[textIndex];
+		9:
+			$TextContainer/MainContainer/ImageContainer.hide();
+		11:
+			$TextContainer/MainContainer/ImageContainer.hide();
+		12:
+			$TextContainer/MainContainer/ImageContainer.hide();
+		13:
+			image.texture = images[8];
+			title.text = titleTexts[textIndex];
+		14:
+			$TextContainer/MainContainer/ImageContainer.hide();
+			title.text = "";
+
 func _on_mascot_animations_animation_finished(anim_name: StringName) -> void:
-	var textIndex: int = int(progressBar.value);
-	title.text = titleTexts[textIndex];
-	description.text = descriptionTexts[textIndex];
+	textIndex = int(progressBar.value);
 	
 	match anim_name:
 		"transition1":
-			mainButton.show();
-			mainButton.text = buttonTexts[0]
+			$TextContainer.add_theme_constant_override("margin_bottom", 32);
+			mainButton.text = buttonTexts[0];
+			title.text = titleTexts[textIndex];
+			description.text = descriptionTexts[textIndex];
 		"transition2":
-			mainButton.show();
 			mainContainer.alignment = BoxContainer.ALIGNMENT_BEGIN;
-			mainButton.text = buttonTexts[1]
-			mainButton.size_flags_horizontal = Control.SIZE_SHRINK_END;
+			mainButton.text = buttonTexts[1];
+			$ButtonContainer.size_flags_horizontal = Control.SIZE_SHRINK_END;
+			title.text = titleTexts[textIndex];
+			description.text = descriptionTexts[textIndex];
 		"transition3":
-			mainButton.hide();
-			mainButton.text = buttonTexts[2]
-			mainButton.size_flags_horizontal = Control.SIZE_SHRINK_CENTER;
+			mainButton.text = buttonTexts[2];
+			title.text = titleTexts[textIndex];
+			description.text = descriptionTexts[textIndex];
+		"transition5":
+			$TextContainer/MainContainer/ImageContainer.show();
+			image.texture = images[5];
+			title.text = titleTexts[textIndex];
+		"transition6":
+			$TextContainer/MainContainer/ImageContainer.show();
+			image.texture = images[6];
+			title.text = titleTexts[textIndex];
+		"transition7":
+			$TextContainer/MainContainer/ImageContainer.show();
+			mainButton.text = buttonTexts[3];
+			image.texture = images[7];
+			title.text = titleTexts[textIndex];
+		"despedida":
+			title.text = titleTexts[textIndex];
+			description.text = descriptionTexts[4];
+			mainButton.text = buttonTexts[4];
+			mainContainer.alignment = BoxContainer.ALIGNMENT_CENTER;
+			$ButtonContainer.size_flags_horizontal = Control.SIZE_SHRINK_CENTER;
+			mainButton.show();
 
 func _on_main_button_pressed() -> void:
-	progressBar.set_value(progressBar.value + 1.0);
-	mainButton.hide();
-	infoButton.hide();
-	
 	title.text = "";
 	description.text = "";
+	progressBar.set_value(progressBar.value + 1.0);
+	mainButton.hide();
+	backButton.hide();
+	infoButton.hide();
--- tutorial-sigeva-unpaz/Scripts/mascot.gd
+++ tutorial-sigeva-unpaz/Scripts/mascot.gd
@@ -1,13 +1,16 @@
 extends TextureRect
 
+@onready var mainMargin: MarginContainer = get_parent().get_node("MainMargin");
 @onready var dialogBox: PanelContainer = $DialogBox;
 @onready var dialogText: RichTextLabel = $DialogBox/BoxMargin/DialogText;
 @onready var mascotAnimations: AnimationPlayer = $MascotAnimations;
 
 const clementinaTextures: Array[Texture2D] = [
 	preload("res://Assets/Clementina_parada.png"),
-	preload("res://Assets/Clementina_saludando.png"),
-	preload("res://Assets/Clementina_pensando.png")
+	preload("res://Assets/Clementina_saludando_der.png"),
+	preload("res://Assets/Clementina_saludando_izq.png"),
+	preload("res://Assets/Clementina_pensando.png"),
+	preload("res://Assets/Clementina_guiñando.png")
 ];
 
 const mascotDialogs: Array[String] = [
@@ -15,12 +18,52 @@ const mascotDialogs: Array[String] = [
 	"[b]¡Bienvenido/a![/b]
 Soy Clementina y voy a ayudarte a completar tu CVar.
 ¿Estás listo/a para comenzar?",
-	"Tener tu [b]CVAR al día[/b] es como tener tu mochila lista:
+	"Tener tu [b]CVar al día[/b] es como tener tu mochila lista:
 ¡todo preparado para tu próximo desafío académico!",
+	"Estos son los pasos que vas a completar.
+Seguí el orden para avanzar en el tutorial
+y lograr tu registro en CVar.",
+	"Este es el [b]formulario de registro[/b].
+Tenés que completarlo con tus
+datos personales para continuar.
+Si venís del exterior y [b]no[/b] contás con
+CUIL/CUIT, no te preocupes: podés
+registrarte con tu [b]Pasaporte.[/b]",
+	"¡Listo! Ahora [b]revisá tu correo:[/b] vas a
+recibir un [b]mail de confirmación[/b] con
+un enlace para continuar.",
+	"Ahora entrarás con el usuario que 
+creaste y [b]la contraseña que te llegó
+por mail.[/b]",
+	"Ahora solo queda [b]iniciar sesión
+nuevamente[/b] con tu [b]nueva
+contraseña[/b]",
+	"Para continuar, elegí la primera opción:
+[b]Usuario banco de datos de actividades
+de CyT[/b]",
+	"Una vez seleccionado el Rol, verás un gran
+formulario para llenar, [b]nos enfocaremos[/b], por 
+ahora, en estas secciones: [b]Datos personales[/b],
+[b]Formación Académica[/b] y [b]Antecedentes[/b].",
+	'Haz click en el [b]botón "Sin datos"[/b]
+para completar con tu información',
+	'Haz click en el [b]botón "0"[/b] para
+completar con tu información',
+	'Para [b]cargar un proyecto[/b] de extensión,
+asegurate de tener a mano la [b]sinopsis[/b] y
+el [b]resumen[/b] del mismo.
+Haz click en el [b]botón "0"[/b] para
+completar con tu información',
+	"Recordá: [b]marcá todas las casillas,
+completá la fecha y descargá tu CV[/b]
+en formato .pdf o .doc y ¡Listo!",
 	"Este diálogo no debería aparecer"
 ];
 
+const tweenDuration: float = 3.0;
+
 var textIndex: int;
+var tween: Tween;
 
 func _ready() -> void:
 	mascotAnimations.play("RESET");
@@ -28,28 +71,119 @@ func _ready() -> void:
 	dialogBox.hide();
 	dialogText.text = mascotDialogs[0];
 
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept") and tween != null:
+		tween.custom_step(tweenDuration);
+
 func _on_progress_bar_value_changed(value: float) -> void:
 	textIndex = int(value);
-	
-	dialogBox.hide();
 	show();
+	dialogBox.hide();
 	texture = clementinaTextures[0];
-	mascotAnimations.play("transition" + str(textIndex));
+	
+	if (textIndex < 4): mascotAnimations.play("transition" + str(textIndex));
+	else:
+		match textIndex:
+			4:
+				texture = clementinaTextures[4];
+				dialogBox.position = Vector2(-384.0, -180.0);
+				dialogBox.size = Vector2(464.0, 100.0);
+				dialogText.size = Vector2(464.0, 100.0);
+				dialogBox.pivot_offset = Vector2(464.0, 128.0);
+				growBubble();
+			5:
+				texture = clementinaTextures[0];
+				dialogBox.position.x = -288.0;
+				growBubble();
+				dialogBox.size = Vector2(460.0, 100.0);
+				dialogText.size = Vector2(460.0, 100.0);
+			6:
+				dialogBox.position = Vector2(-352.0, -118.0);
+				#dialogBox.size = Vector2(460.0, 100.0);
+				#dialogText.size = Vector2(460.0, 100.0);
+				growBubble();
+			7:
+				texture = clementinaTextures[2];
+				dialogBox.position = Vector2(-288.0, -160.0);
+				dialogBox.size = Vector2(406.0, 100.0);
+				dialogText.size = Vector2(406.0, 100.0);
+				growBubble();
+			8:
+				dialogBox.position = Vector2(-288.0, -180.0);
+				dialogBox.size = Vector2(500.0, 100.0);
+				dialogText.size = Vector2(500.0, 100.0);
+				dialogBox.pivot_offset = Vector2(460.0, 160.0);
+				growBubble();
+			9:
+				texture = clementinaTextures[0];
+				dialogText.size = Vector2(570.0, 200.0);
+				dialogBox.size = Vector2(570.0, 200.0);
+				dialogBox.pivot_offset = Vector2(0.0, 128.0);
+				mascotAnimations.play("transition4");
+			10:
+				dialogBox.pivot_offset = Vector2(448.0, 100.0);
+				mascotAnimations.play("transition5");
+			11:
+				flip_h = false;
+				dialogBox.pivot_offset = Vector2(390.0, 100.0);
+				mascotAnimations.play("transition6");
+			12:
+				texture = clementinaTextures[0];
+				dialogBox.pivot_offset = Vector2(416.0, 224.0);
+				dialogText.size = Vector2(506.0, 100.0);
+				dialogBox.size = Vector2(506.0, 100.0);
+				mascotAnimations.play("transition7");
+			13:
+				texture = clementinaTextures[0];
+				dialogBox.position = Vector2(-224.0, -186.0);
+				dialogBox.pivot_offset = Vector2(232.0, 180.0);
+				growBubble();
+				dialogText.size = Vector2(464.0, 100.0);
+				dialogBox.size = Vector2(464.0, 100.0);
+			14:
+				mascotAnimations.play("despedida");
 
 func _on_mascot_animations_animation_finished(anim_name: StringName) -> void:
-	if (anim_name != "RESET" and anim_name != "growBubble"):
-		dialogText.text = mascotDialogs[textIndex];
-		dialogBox.pivot_offset =  dialogBox.position.direction_to(position) * dialogBox.size;
-		dialogText.visible_ratio = 0.0;
-		mascotAnimations.play("growBubble");
+	if (anim_name != "RESET" and anim_name != "growBubble" and anim_name != "despedida"):
+		growBubble();
 	if (anim_name == "growBubble"): showDialog();
 	match anim_name:
 		"transition1":
 			texture = clementinaTextures[1];
+			dialogBox.size = Vector2(654.0, 166.0);
+			dialogText.size = Vector2(654.0, 166.0);
+			dialogBox.pivot_offset = Vector2(0.0, 119.0);
 		"transition2":
+			texture = clementinaTextures[3];
+			dialogBox.size = Vector2(634.0, 100.0);
+			dialogText.size = Vector2(634.0, 100.0);
+			dialogBox.pivot_offset = Vector2(0.0, 59.5);
+		"transition3":
+			texture = clementinaTextures[2];
+			dialogBox.size = Vector2(518.0, 100.0);
+			dialogText.size = Vector2(518.0, 100.0);
+			dialogBox.pivot_offset = Vector2(518.0, 128.0);
+		"transition5":
+			texture = clementinaTextures[3];
+			dialogText.size = Vector2(448.0, 100.0);
+			dialogBox.size = Vector2(448.0, 100.0);
+			flip_h = true;
+		"transition6":
+			texture = clementinaTextures[2];
+			dialogText.size = Vector2(390.0, 100.0);
+			dialogBox.size = Vector2(390.0, 100.0);
+		"transition7":
 			texture = clementinaTextures[2];
 
+func growBubble() -> void:
+	dialogText.text = mascotDialogs[textIndex];
+	dialogText.visible_ratio = 0.0;
+	mascotAnimations.play("growBubble");
+
 func showDialog() -> void:
-	var tween = create_tween();
-	tween.tween_method(dialogText.set_visible_ratio, 0.0, 1.0, 3.0);
+	tween = create_tween();
+	tween.tween_method(dialogText.set_visible_ratio, 0.0, 1.0, tweenDuration);
+	tween.finished.connect(tween.kill);
+	tween.finished.connect(mainMargin.mainButton.show);
+	tween.finished.connect(mainMargin.backButton.show);
 	tween.play();
