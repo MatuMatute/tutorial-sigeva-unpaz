bd3818d307213c849eaa8cbe863f97eaf28fa0f6
Matías Sandoval <matiasgonzalezsandoval2@gmail.com>
2025-10-10 20:36:27 -0300

    v1.0.2
    - Botón de atrás en los créditos

--- tutorial-sigeva-unpaz/Scripts/main.gd
+++ tutorial-sigeva-unpaz/Scripts/main.gd
@@ -2,12 +2,11 @@ extends Control
 
 @onready var creditsAnimation: AnimationPlayer = $MainAnimation;
 
-func _process(_delta: float) -> void:
-	if ($Credits.modulate.a == 1.0 and Input.is_action_pressed("ui_accept")):
-		creditsAnimation.play("fade_out");
-
 func _on_info_pressed() -> void:
 	creditsAnimation.play("fade_in");
+
+func _on_back_pressed() -> void:
+	creditsAnimation.play("fade_out");
 
 func _on_mute_toggled(toggled_on: bool) -> void:
 	AudioServer.set_bus_mute(0, toggled_on);
